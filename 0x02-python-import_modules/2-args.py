18c2eb2ece9dda02f849d6222926358882e08bcb
Babatunde Adeoti <babaolu98@gmail.com>
2023-06-09 14:48:08 +0100

    Accounting for only one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
+        print("1 argument.")
+    elif len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv)-1))
         count = 1
         for _ in sys.argv[1:]:
