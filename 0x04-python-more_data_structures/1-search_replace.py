b91bd7a264f92fc80da0cb3b9382ed1863bffff9
Babatunde Adeoti <babaolu98@gmail.com>
2023-06-14 23:00:44 +0100

    Replaces all occurrence of an element

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+    s_count =  my_list.count(search)
+    if s_count == 0:
+        return my_list
+    else:
+        new_list = my_list[:]
+        for _ in range(s_count):
+            new_list[new_list.index(search)] = replace
+        return new_list
