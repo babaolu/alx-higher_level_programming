1c5f2f3028a597c72f8a6755fe82ae0684e76922
Babatunde Adeoti <babaolu98@gmail.com>
2023-06-15 03:03:51 +0100

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    weight_add = 0
+    freq = 0
+    for x, y in my_list:
+        weight_add += x * y
+        freq += y
+    return weight_add/freq
