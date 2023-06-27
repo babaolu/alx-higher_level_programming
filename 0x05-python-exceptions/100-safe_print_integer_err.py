f57c565f46480fb54ff8fe3a57e3ddcdfc38f58a
Babatunde Adeoti <babaolu98@gmail.com>
2023-06-27 03:28:24 +0100

    Adding two blank lines to satisfy pycodestyle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
-#        intval = int(value)
         print("{:d}".format(value))
         return True
     except Exception as err:
