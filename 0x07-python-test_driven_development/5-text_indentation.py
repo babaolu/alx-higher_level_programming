ca819aa7df5fbe45bd9d8b6c6c2655acd5e5da73
Babatunde Adeoti <babaolu98@gmail.com>
2023-07-14 16:21:26 +0100

    Indenting text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module implements a function that indents texts
+"""
+
+
+def text_indentation(text=''):
+    """ Add 2 newlines after . ? and : """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.strip()
+    for i in ".?:":
+        text = text.replace(i + " ", i + "\n\n")
+    print(text, end="")
