dfc2307c800ffae01083729ac27694da70dd6b3e
Babatunde Adeoti <babaolu98@gmail.com>
2023-07-15 17:25:47 +0100

    Testing for max at end

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,5 +14,6 @@ class MaxIntTest(unittest.TestCase):
         self.assertEqual(max_integer((3, 7, 12, 5)), 12)
         self.assertEqual(max_integer([2]), 2)
         self.assertEqual(max_integer([4, 4, 4]), 4)
+        self.assertEqual(max_integer([7, 4, 14]), 14)
         self.assertRaises(TypeError, max_integer, {45, 6, 211, 5})
         self.assertRaises(TypeError, max_integer, 3)
