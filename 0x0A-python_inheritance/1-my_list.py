8482f543d9119636a38da5a6fb407c58ced7a508
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-11 22:08:17 +0100

    Satisfying pycodestyle

--- 0x0A-python_inheritance/1-my_list.py
+++ 0x0A-python_inheritance/1-my_list.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
+
 """ This module implements a list inheritor """
 
-class MyList(list):
-	"""
-	Class that inherits list
-	"""
 
-	def print_sorted(self):
-		"""
-		Prints sorted list
-		"""
-		self.sort()
-		print(self)
+class MyList(list):
+    """
+    Class that inherits list
+    """
+
+    def print_sorted(self):
+        """
+        Prints sorted list
+        """
+        self.sort()
+        print(self)
