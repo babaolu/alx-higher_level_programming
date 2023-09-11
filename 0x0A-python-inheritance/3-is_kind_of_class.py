e1c121639b92644d34ddd58ac4a9d4ef002838be
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-11 22:32:57 +0100

    Base class now accounted for

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module implements is_kind_of_class function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if obj is an instance of a_class
+    """
+    return isinstance(obj, a_class)
