a4aca57ed10694383656026fc1ce83a99abd33e0
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-11 23:00:28 +0100

    Adding function that always throws

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module implements class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Class BaseGeometry"""
+    def area(self):
+        """ Raises exception """
+        raise Exception("area() is not implemented")
