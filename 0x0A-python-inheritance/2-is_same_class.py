a816c50b772b81153d7c040688f078830cd91108
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-11 22:20:27 +0100

    Compare type with class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module implements is_same_class function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if obj is an instance of a_class
+    """
+    return isinstance(obj, a_class)
