99364dd1fa62dad2ea3e40162d2b56281716969a
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-12 04:54:07 +0100

    Testing area without implementing

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This implements the Square class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square """
+    def __init__(self, size):
+        """ Instantiated Square class """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
