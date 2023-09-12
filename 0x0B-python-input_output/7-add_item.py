2fd5781e34d0653dc8595f9d65781a6011fd5c2e
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-12 23:57:18 +0100

    Handling exception when file not found

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-if len(sys.argv) > 1:
+try:
     my_list = load_from_json_file("add_item.json")
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, "add_item.json")
-else:
-    save_to_json_file([], "add_item.json")
+except FileNotFoundError:
+    my_list = []
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, "add_item.json")
