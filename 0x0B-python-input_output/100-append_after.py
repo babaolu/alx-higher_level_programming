0aa99171f49ffa2e470d8884e75ad73e243a44fe
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-13 11:58:14 +0100

    Inserts a line into file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Implements function for inserting text """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Inserts line of text after a line containing a string """
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        line_list = f.readlines()
+        for i in range(len(line_list)):
+            if search_string in line_list[i]:
+                line_list.insert(i+1, new_string)
+        f.seek(0)
+        f.writelines(line_list)
