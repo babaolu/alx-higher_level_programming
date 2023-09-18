f3a10658b691fb0ac484a8c37f70d7833118373c
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-18 11:41:44 +0100

    Writing JSON string representations to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ This implements the Base class """
+import json
 
 
 class Base:
@@ -17,3 +18,25 @@ class Base:
     @classmethod
     def nb_objects(cls):
         return cls.__nb_objects
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        with open(cls.__name__ + ".json", mode="w", encoding="utf-8") as f:
+            if not list_objs:
+                f.write("[]")
+                return
+            myl = [x.__dict__ for x in list_objs]
+            new_dict = {}
+            for i in range(len(myl)):
+                for key, value in myl[i].items():
+                    new_dict[key.removeprefix("_" + type(list_objs[i]).__name__
+                             + "__")] = value
+                myl[i] = new_dict
+                new_dict = {}
+            json.dump(myl, f)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
