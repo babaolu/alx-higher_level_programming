0ea54b890cadb0eb1a7b068ecedd2ead2cab18eb
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-18 21:28:46 +0100

    Making tests work

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,4 +7,6 @@ from models.rectangle import Rectangle
 class TestRectangleClass(unittest.TestCase):
     """ Rectangle test implementation """
 
-    def test_x(self):
+    def test_init(self):
+        rect = Rectangle(3, 5)
+        self.assertEqual(rect.area(), 15)
