0db5b3cf805c2f08801f18e1404efb15a3c2f9d0
Babatunde Adeoti <babaolu98@gmail.com>
2023-09-19 05:50:34 +0100

    Instance test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,3 +10,11 @@ class TestRectangleClass(unittest.TestCase):
     def test_init(self):
         rect = Rectangle(3, 5)
         self.assertEqual(rect.area(), 15)
+        self.assertRaises(TypeError, Rectangle, "4", 5)
+        self.assertRaises(TypeError, Rectangle, 4, "5")
+        self.assertRaises(ValueError, Rectangle, 0, 1)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+
+    def test_create(self):
+        self.assertNotEqual(Rectangle.create(width=1, height=3).__dict__,
+                            Rectangle(1, 3).__dict__)
