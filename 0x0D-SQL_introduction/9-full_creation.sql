f2b0c9085ebd9bd2654a55568cf442df56c65342
Babatunde Adeoti <babaolu98@gmail.com>
2023-10-17 23:44:47 +0100

    correcting error in task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates and populate table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VACHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 	VALUES ROW(1, "John", 10), ROW(2, "Alex", 3),
 	ROW( 3, "Bob", 14), ROW(4, "George", 8);
