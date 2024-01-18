c0115e44dd6e4fa3eedf6ef0c1d3b6815f95376b
Babatunde Adeoti <babaolu98@gmail.com>
2024-01-18 22:04:20 +0100

    Joining tvshows and genres

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Querying list of all cities int hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 	FROM cities INNER JOIN states
-	ON cities.state_id = states.id ORDER BY cities.id
+	ON cities.state_id = states.id ORDER BY cities.id;
