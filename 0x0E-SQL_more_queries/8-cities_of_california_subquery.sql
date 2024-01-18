370e31e4ae4658d568465325e387f7d8541caa14
Babatunde Adeoti <babaolu98@gmail.com>
2024-01-18 15:38:06 +0100

    Last correction for 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Queries cities of California
 SELECT * FROM cities WHERE state_id = 
-	(SELECT id FROM states WHERE name = "California") ORDER BY cities.id ASC;
+	(SELECT id FROM states WHERE name = "California") ORDER BY cities.id;
