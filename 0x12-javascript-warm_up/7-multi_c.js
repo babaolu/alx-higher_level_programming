3888c594c53a06239b0c01a2fcfd4a56ec046d52
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-13 01:33:39 +0100

    Print C based on commandline specified count value

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const count = parseInt(process.argv[2], 10);
+if (!count) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
