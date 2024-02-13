a5287282d16a2ff7af55c83c7615f886e43ad309
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-13 02:14:36 +0100

    Prints a square with size specified by commandline argument

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const count = parseInt(process.argv[2], 10);
-if (!count) {
+if (!(count || count === 0)) {
   console.log('Missig size');
 } else {
   for (let i = 0; i < count; i++) {
