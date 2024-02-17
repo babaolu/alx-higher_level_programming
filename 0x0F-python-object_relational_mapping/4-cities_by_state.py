4f8811d7bb4fafec1074713c5c8417de7871c623
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-17 22:20:25 +0100

    Querying cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Querying from states table in a specified database """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                           passwd=args[2], db=args[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities" +
+                " INNER JOIN states ON cities.state_id = states.id" +
+                " ORDER BY states.id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
