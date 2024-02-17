9f41c1c01020012f3b0929e68b93de905c7c212b
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-17 19:18:07 +0100

    Filters out those not starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Querying from states table in a specified database """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                           passwd=args[2], db=args[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * from states ORDER BY states.id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == "N":
+            print(row)
+    cur.close()
+    conn.close()
