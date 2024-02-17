d5bdab40e02f54270f3868e7e9a7d3c0d7442abc
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-17 19:56:59 +0100

    Filters out state not specified in the command line

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
                            passwd=args[2], db=args[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * from states WHERE name = '{}' ".format(args[4]) +
-                "ORDER BY states.id ASC;")
+    cur.execute("SELECT * from states WHERE states.name = \"{}\" "
+                .format(args[4]) + "ORDER BY states.id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
