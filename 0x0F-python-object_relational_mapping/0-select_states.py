ff7844ee4f4aab3f146fb2a8d94ff617cecdc488
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-17 16:39:42 +0100

    Correction for pycodestyle compliance

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Querying from states table in a specified database """
 if __name__ != "__main__":
     exit()
 import MySQLdb
@@ -6,7 +7,7 @@ import sys
 
 args = sys.argv
 conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
-                     passwd=args[2], db=args[3], charset="utf8")
+                       passwd=args[2], db=args[3], charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * from states ORDER BY states.id")
 query_rows = cur.fetchall()
