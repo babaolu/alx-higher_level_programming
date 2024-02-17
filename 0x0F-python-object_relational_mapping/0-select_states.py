8f850cf77fa5c374dc8bcac5dc25896119c16511
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-17 18:58:30 +0100

    Adding semicolon

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ args = sys.argv
 conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
                        passwd=args[2], db=args[3], charset="utf8")
 cur = conn.cursor()
-cur.execute("SELECT * from states ORDER BY states.id")
+cur.execute("SELECT * from states ORDER BY states.id;")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
