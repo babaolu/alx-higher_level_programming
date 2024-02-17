37d5ac6dbc963d637de07f2a81947484c1ef7d0e
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-17 21:33:25 +0100

    Making arguments safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Querying from states table in a specified database """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+    fname = args[4].split(sep=";")[0].rstrip("\"")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                           passwd=args[2], db=args[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * from states WHERE states.name = \"{}\" "
+                .format(fname) +
+                "ORDER BY states.id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
