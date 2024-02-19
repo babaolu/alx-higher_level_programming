d077be2bbbb7200c0d1685b58fed13101e7ea96a
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-19 11:39:48 +0100

    Adding a new state to the database

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,14 +10,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    fname = sys.argv[4].split(sep=";")[0].rstrip("\"")
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instances = (session.query(State.id)
-                 .filter(State.name.in_([fname])))
-    if instances.first():
-        for instance in instances:
-            print(instance.id)
-    else:
-        print("Not Found")
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
