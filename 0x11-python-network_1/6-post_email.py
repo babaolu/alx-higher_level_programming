90cab101cf61ed882658312e15d2eb787da9bd8d
Babatunde Adeoti <babaolu98@gmail.com>
2024-03-02 04:12:30 +0100

    POSTing with requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    res = requests.get(sys.argv[1])
-    print(res.headers.get("X-Request-Id"))
+    values = {"email": sys.argv[2]}
+    post = requests.post(sys.argv[1], values)
+    print(post.text)
