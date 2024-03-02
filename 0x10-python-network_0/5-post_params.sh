ba630091f2f3a3fcc92aa11d7e4a854677d704e7
Babatunde Adeoti <babaolu98@gmail.com>
2024-03-02 01:30:48 +0100

    Finding peak

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays size of http response to a request
-curl -s -X POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module implements a function that find a peak """
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return
+    myl = list_of_integers
+    peak = myl[0]
+    state = 0
+    for i in range(1, len(list_of_integers)):
+        if state:
+            if myl[i] < myl[i - 1]:
+                return myl[i - 1]
+            else:
+                peak = myl[i]
+        else:
+            if myl[i] > myl[i - 1]:
+                state = 1
+                peak = myl[i]
+    return peak
