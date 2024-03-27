5807a982749cfeffcf2f43ba33f5736a7a56816b
Babatunde Adeoti <babaolu98@gmail.com>
2024-03-27 05:57:09 +0100

    Print character

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const num = process.argv[2];
+request('https://swapi-api.alx-tools.com/api/films/', (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Status code:', response.statusCode);
+    return;
+  }
+
+  const info = JSON.parse(body);
+  for (let j = 0; j < info.results[num].characters.length; j++) {
+    request(info.results[num].characters[j], (cerror, cres, cbody) => {
+      if (cerror) {
+        console.error('CError:', cerror);
+        return;
+      }
+      if (cres.statusCode !== 200) {
+        console.error('CStatus code:', cres.statusCode);
+        return;
+      }
+      const cinfo = JSON.parse(cbody);
+      console.log(cinfo.name);
+    });
+  }
+});
